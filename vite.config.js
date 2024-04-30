437ad29d36d41134eab7f618229c543c568f4d41
ThePrince-Verma <prince.pv759@gmail.com>
2024-04-30 15:11:58 +0530

    i comment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project-3-calculator/',
-  plugins: [react()],
-})
+  plugins: [
+    reactRefresh(),
+    VitePWA()
+  ]
+});
